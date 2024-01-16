e39cc1baeb0b9463372eed0836df5f7d43a0a53c
Martin <emailregisterlogin@gmail.com>
2024-01-16 04:19:05 +0100

    Added updated gif/pic of serious game

--- data.js
+++ data.js
@@ -1,110 +1,109 @@
-var json_object = 
-[
+var json_object = [
   {
-    "title": "University Project: Motor Rehabiliation Serious Game",
-    "image_folder": "serious_game",
-    "images": ["1.gif"],
-    "completion": "Ongoing project (November 2023 → present)"
+    title: "University Project: Motor Rehabiliation Serious Game",
+    image_folder: "serious_game",
+    images: ["1.gif", "2.png", "3.gif"],
+    completion: "Ongoing project (November 2023 → present)",
   },
   {
-    "title": "Personal Project: Custom C++ Game Engine",
-    "link": "https://github.com/martinstarkov/protegon",
-    "image_folder": "game_engine",
-    "images": ["1.gif", "2.gif", "3.gif", "4.gif", "5.gif", "6.gif", "7.gif"],
-    "completion": "Ongoing long-term project (2019 → present)"
+    title: "Personal Project: Custom C++ Game Engine",
+    link: "https://github.com/martinstarkov/protegon",
+    image_folder: "game_engine",
+    images: ["1.gif", "2.gif", "3.gif", "4.gif", "5.gif", "6.gif", "7.gif"],
+    completion: "Ongoing long-term project (2019 → present)",
   },
   {
-    "title": "Personal Project: Slingshot",
-    "image_folder": "slingshot",
-    "images": ["1.gif", "2.jpg", "3.gif", "4.jpg"],
-    "completion": "Completed in August, 2023"
+    title: "Personal Project: Slingshot",
+    image_folder: "slingshot",
+    images: ["1.gif", "2.jpg", "3.gif", "4.jpg"],
+    completion: "Completed in August, 2023",
   },
   {
-    "title": "GMTK 2023 Game Jam Entry: Tower Offense",
-    "link": "https://bicyclemice.itch.io/tower-offense",
-    "image_folder": "gmtk_jam_2023",
-    "images": ["1.png", "2.png", "3.png"],
-    "completion": "Completed in July, 2023"
+    title: "GMTK 2023 Game Jam Entry: Tower Offense",
+    link: "https://bicyclemice.itch.io/tower-offense",
+    image_folder: "gmtk_jam_2023",
+    images: ["1.png", "2.png", "3.png"],
+    completion: "Completed in July, 2023",
   },
   {
-    "title": "Rocketry Team: Thrust-Vector-Controlled Lander Vehicle",
-    "image_folder": "bayes_tvc",
-    "images": ["1.gif", "2.gif", "3.gif", "4.png"],
-    "completion": "Continued by successor after June, 2023"
+    title: "Rocketry Team: Thrust-Vector-Controlled Lander Vehicle",
+    image_folder: "bayes_tvc",
+    images: ["1.gif", "2.gif", "3.gif", "4.png"],
+    completion: "Continued by successor after June, 2023",
   },
   {
-    "title": "BEng Thesis: Hybrid Aerial-Aquatic Vehicle",
-    "link": "modules/thesis/BEng_thesis.pdf",
-    "image_folder": "bachelor_thesis",
-    "images": ["1.gif", "2.png", "3.jpg", "4.jpg", "5.png", "6.png"],
-    "completion": "Completed in May, 2023"
+    title: "BEng Thesis: Hybrid Aerial-Aquatic Vehicle",
+    link: "modules/thesis/BEng_thesis.pdf",
+    image_folder: "bachelor_thesis",
+    images: ["1.gif", "2.png", "3.jpg", "4.jpg", "5.png", "6.png"],
+    completion: "Completed in May, 2023",
   },
   {
-    "title": "Internship: JST Electrical Connector Vibration Reduction",
-    "image_folder": "connector_internship",
-    "images": ["1.png", "2.png", "3.png", "4.png"],
-    "completion": "Completed in February, 2023"
+    title: "Internship: JST Electrical Connector Vibration Reduction",
+    image_folder: "connector_internship",
+    images: ["1.png", "2.png", "3.png", "4.png"],
+    completion: "Completed in February, 2023",
   },
   {
-    "title": "Engineering Group Thesis: Metamaterial CVT Gear System",
-    "link": "modules/group_thesis/hoberman_group_thesis.pdf",
-    "image_folder": "hoberman_thesis",
-    "images": [["1.gif", "padding: 17.4% 0%;"]],
-    "completion": "Completed in November, 2022"
+    title: "Engineering Group Thesis: Metamaterial CVT Gear System",
+    link: "modules/group_thesis/hoberman_group_thesis.pdf",
+    image_folder: "hoberman_thesis",
+    images: [["1.gif", "padding: 17.4% 0%;"]],
+    completion: "Completed in November, 2022",
   },
   {
-    "title": "Personal Project: Robust Collision System",
-    "link": "https://github.com/martinstarkov/protegon/blob/main/src/physics/collision.cpp",
-    "image_folder": "collision_detection",
-    "images": ["1.png", "2.gif", "3.gif", "4.gif", "5.gif"],
-    "completion": "Completed in September, 2022"
+    title: "Personal Project: Robust Collision System",
+    link: "https://github.com/martinstarkov/protegon/blob/main/src/physics/collision.cpp",
+    image_folder: "collision_detection",
+    images: ["1.png", "2.gif", "3.gif", "4.gif", "5.gif"],
+    completion: "Completed in September, 2022",
   },
   {
-    "title": "Personal Project: Cache-Friendly ECS C++ Library",
-    "link": "https://github.com/martinstarkov/ecs",
-    "image_folder": "ecs_library",
-    "images": ["1.png"],
-    "completion": "Completed in August, 2022"
+    title: "Personal Project: Cache-Friendly ECS C++ Library",
+    link: "https://github.com/martinstarkov/ecs",
+    image_folder: "ecs_library",
+    images: ["1.png"],
+    completion: "Completed in August, 2022",
   },
   {
-    "title": "GMTK 2022 Game Jam Entry: Stroll of the Dice",
-    "link": "https://bicyclemice.itch.io/stroll-of-the-dice",
-    "image_folder": "gmtk_jam_2022",
-    "images": ["1.gif", "2.png"],
-    "completion": "Completed in July, 2022"
+    title: "GMTK 2022 Game Jam Entry: Stroll of the Dice",
+    link: "https://bicyclemice.itch.io/stroll-of-the-dice",
+    image_folder: "gmtk_jam_2022",
+    images: ["1.gif", "2.png"],
+    completion: "Completed in July, 2022",
   },
   {
-    "title": "Codeplay Project: GPU Optimized Fluid Simulation",
-    "link": "https://github.com/codeplaysoftware/computecpp-sdk/tree/master/demos/fluid",
-    "image_folder": "fluid_simulation",
-    "images": ["1.gif", "2.gif"],
-    "completion": "Completed in June, 2022"
+    title: "Codeplay Project: GPU Optimized Fluid Simulation",
+    link: "https://github.com/codeplaysoftware/computecpp-sdk/tree/master/demos/fluid",
+    image_folder: "fluid_simulation",
+    images: ["1.gif", "2.gif"],
+    completion: "Completed in June, 2022",
   },
   {
-    "title": "University Project: Wind Turbine Build",
-    "image_folder": "wind_turbine",
-    "images": ["1.gif", "2.gif", "3.png"],
-    "completion": "Completed in April, 2022"
+    title: "University Project: Wind Turbine Build",
+    image_folder: "wind_turbine",
+    images: ["1.gif", "2.gif", "3.png"],
+    completion: "Completed in April, 2022",
   },
   {
-    "title": "University Project: Gear Box",
-    "image_folder": "gear_box",
-    "images": ["1.gif"],
-    "completion": "Completed in December, 2021"
+    title: "University Project: Gear Box",
+    image_folder: "gear_box",
+    images: ["1.gif"],
+    completion: "Completed in December, 2021",
   },
   {
-    "title": "University Project: Chemical Spill Simulation",
-    "link": "https://github.com/martinstarkov/cmm3-project",
-    "image_folder": "chemical_spill_simulation",
-    "images": ["1.gif", "2.gif"],
-    "completion": "Completed in November, 2021"
+    title: "University Project: Chemical Spill Simulation",
+    link: "https://github.com/martinstarkov/cmm3-project",
+    image_folder: "chemical_spill_simulation",
+    images: ["1.gif", "2.gif"],
+    completion: "Completed in November, 2021",
   },
   {
-    "title": "Robotics Team Project: CreatED Hack 2020 Website",
-    "link": "https://createdhack.github.io/",
-    "image_folder": "created_hack",
-    "images": ["1.gif", "2.gif"],
-    "completion": "Completed in January, 2020"
+    title: "Robotics Team Project: CreatED Hack 2020 Website",
+    link: "https://createdhack.github.io/",
+    image_folder: "created_hack",
+    images: ["1.gif", "2.gif"],
+    completion: "Completed in January, 2020",
   },
-  {}
-]
+  {},
+];
